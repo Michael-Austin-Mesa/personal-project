6f2deb6ad4b1abf6453ce61f8982b60447aa38f8
Michael Mesa <michael.austin.mesa@gmail.com>
2022-12-15 12:51:33 -0600

    finalized explore.py file

---
+++ explore.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+import requests
+
+import pandas as pd
+import numpy as np
+from scipy import stats
+from math import sqrt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import statsmodels.api as sm
+from statsmodels.tsa.api import Holt
+
+from datetime import datetime
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import TimeSeriesSplit 
+
+
+import wrangle as w
+
+
+# In[2]:
+
+
+btc = w.get_crypto_price('btc', '2018-01-01', '2022-12-12')
+
+
+# In[3]:
+
+
+btc = w.clean_data(btc)
+
+
+# In[4]:
+
+
+train = btc[:'2020']
+validate = btc['2021']
+test = btc['2022']
+
+
+# In[5]:
+
+
+y = train.btc_volume
+
+
+# In[6]:
+
+
+def get_avg_vol_monthly():
+    sns.set(rc={'figure.figsize': (10, 6)})
+    sns.set_theme(style="whitegrid")
+    ax = y.groupby([y.index.year, y.index.month]).mean().unstack(0).plot()
+    ax.set(title="Average Volume by Month", xlabel='Month', ylabel='Volume')
+    ax.legend(title='Year')
+    plt.show()
+
+
+# In[7]:
+
+
+def get_vol_by_date():
+    sns.set(rc={'figure.figsize': (10, 6)})
+    sns.set_theme(style="whitegrid")
+    y.groupby([y.index.year]).plot(title= 'Volume by Date', xlabel='Date (Day)', ylabel = 'Volume', legend=True)
+    plt.show()
+
+
+# In[8]:
+
+
+def plot_price_vol():
+    sns.set(rc={'figure.figsize': (10, 6)})
+    sns.set_theme(style="whitegrid")
+    plot_df = train[['btc_open', 'btc_close','btc_high','btc_low']]
+    for col in plot_df.columns:
+        sns.scatterplot(x=plot_df[col], y=y)
+        plt.show()
+
